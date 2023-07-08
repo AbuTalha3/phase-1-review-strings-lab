06cb84294c628fead64618b9b0731df9826a6f4e
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-07-08 12:52:09 +0500

    Update

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code in this file!
+let currentUser = "AbuTalha";
+// let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+console.log(welcomeMessage);
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+console.log(excitedWelcomeMessage);
+
+let  shortGreeting = `Welcome, ${currentUser[0]}!`
+console.log(shortGreeting);
+
+let  shortGreeting2 = `Welcome, ${currentUser.charAt(0)}!`
+console.log(shortGreeting2);
+
+let  shortGreeting3 = `Welcome, ${currentUser.slice(0,1)}!`
+console.log(shortGreeting3);
